5a3ac9a908c7c7fdd36a7b7dcdc24b54fc9ed72d
Muqimjon <58146206+Muqimjon@users.noreply.github.com>
2023-08-10 12:23:19 +0500

    Update Program.cs

--- CineMate.View/Program.cs
+++ CineMate.View/Program.cs
@@ -1,22 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using CineMate.View.IViews.Commons;
-using CineMate.View.Views.Commons;
-
-
-/*IUnitOfWork unitOfWork = new UnitOfWork();
-await unitOfWork.UserRepository.CreateAsync(new User()
-{
-    DateOfBirth = new DateTimeOffset(new DateTime(2006, 10, 13)).UtcDateTime,
-    Email = "muqimjon.mr@Gmail.com",
-    FirstName = "Muqimjon",
-    LastName = "Mamadaliyev",
-    Phone = "+998937349808",
-});
-await unitOfWork.SaveAsync();*/
-
-
-
-IUnitOfView unitOfView = new UnitOfView();
-
-
-await unitOfView.UserServiceView.CreateAsync();
+Console.WriteLine("Hello World");
